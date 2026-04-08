028f5e92bab43686f933c2da5570795c5bf09064
AKASH D <akash@AKASHs-MacBook-Pro.local>
2026-04-08 08:57:58 +0530

    first upload

---
+++ P2.py
@@ -0,0 +1,10 @@
+name = input("Name: ")
+surname = input("Surname: ")
+fullName = name +" "+ surname
+print(fullName)
+message = "Warning! "*2
+print(message)
+print(message.upper())
+print(message.lower())
+print(message.strip())
+print(message.replace("Warning", "Error"))
