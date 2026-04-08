324bfc34c84fc4b43e7d752430f36081202fc2a1
AKASH DAPALAPUR <dapalapurakash02@gmail.com>
2026-04-08 09:45:13 +0530

    Delete first.py

--- first.py
+++
@@ -1,7 +0,0 @@
-boy_name = input("Boy Name: ")
-boy_age = int(input("Boy Age: "))
-girl_name = input("Girl Name: ")
-girl_age = int(input("Girl Age: "))
-# use of abs it prevents the ans in negative
-age_diff = abs(boy_age - girl_age)
-print(f"{boy_name} loves {girl_name}. Age Difference {age_diff}")
